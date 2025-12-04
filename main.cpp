3577cb83896232e5b51ed842c3aa0888ec6c5b3b
Daylen <daylencsmith@gmail.com>
2025-12-03 17:29:56 -0800

    do stuff to use wsl on vscode

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     try {
-        std::string filePath = "test.txt";
+        std::string filePath = argv[1];
         if (argc >= 2) {
             filePath = argv[1];
         }
