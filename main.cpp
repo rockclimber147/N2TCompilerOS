c576972b817686503d633a1c167aec0d08980271
Daylen Smith <daylencsmith@gmail.com>
2025-12-15 22:12:24 +0000

    add assembler to pipeline

--- main.cpp
+++ main.cpp
@@ -2,45 +2,54 @@
 #include <vector>
 #include <string>
 #include <memory>
-#include "parser.hpp" // Included for context, but not used by VMTranslator directly now
-#include "VMTranslator/VMTranslator.hpp" // <-- NEW INCLUDE
+#include "VMTranslator/VMTranslator.hpp"
+#include "HackAssembler/HackAssembler.hpp"
 
 using namespace std;
 
 // --- Define file and directory constants ---
 const string FILENAME = "BasicTest";
 const string INPUT_DIR = "../__input__/";
-const string OUTPUT_DIR = "../__output__"; 
+const string OUTPUT_DIR = "../__output__/"; 
 
 
 int main(int argc, char* argv[]) {
-    // We will ignore command-line arguments (argc, argv) for this fixed test.
     try {
-        cout << __FILE__ << endl;
         cout << "--- Starting VM Translation: " << FILENAME << ".vm ---" << endl;
-        
-        // 1. Instantiate the VMTranslator
-        // VMTranslator(fileName, inputDir, outputDir, debugMode)
         VMTranslator translator(
             FILENAME, 
             INPUT_DIR, 
-            OUTPUT_DIR, 
-            true // Enable Debug Mode for testing
+            OUTPUT_DIR + "asm/", 
+            false
         );
 
-        // 2. Execute the translation process
         translator.translate();
 
         // 3. Close and confirm
         cout << "\n--- Translation Complete ---" << endl;
         cout << "Input: " << INPUT_DIR << FILENAME << ".vm" << endl;
         cout << "Output: " << OUTPUT_DIR << "/asm/" << FILENAME << ".asm" << endl;
+
+                cout << "--- Starting Assembly: " << FILENAME << ".vm ---" << endl;
+        HackAssembler assembler(
+            FILENAME, 
+            OUTPUT_DIR + "asm/", 
+            OUTPUT_DIR + "hack/", 
+            true
+        );
+
+        assembler.assemble();
+
+        // 3. Close and confirm
+        cout << "\n--- Assembly ---" << endl;
+        cout << "Input: " << OUTPUT_DIR << "/asm/" << FILENAME << ".asm" << endl;
+        cout << "Output: " << OUTPUT_DIR << "/asm/" << FILENAME << ".hack" << endl;
         
     }
     catch (const std::runtime_error& e) {
         cerr << "\n!!! Fatal Runtime Error during Translation !!!" << endl;
         cerr << "Error: " << e.what() << endl;
-        return 1; // Return non-zero to indicate failure
+        return 1;
     }
     // Return 0 if successful
     return 0;
