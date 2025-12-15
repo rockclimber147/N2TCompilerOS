1c7504711a2c6604035addf7160dff1d9faf2c3b
Daylen Smith <daylencsmith@gmail.com>
2025-12-15 01:19:26 +0000

    add command type cenum

--- include/VMTranslator/VMSpecififcations.hpp
+++ include/VMTranslator/VMSpecififcations.hpp
@@ -7,13 +7,27 @@
 
 class VMSpecifications {
 public:
+
+    enum class CommandType {
+        C_ARITHMETIC,
+        C_PUSH,
+        C_POP,
+        C_LABEL,
+        C_GOTO,
+        C_IF_GOTO,
+        C_CALL,
+        C_FUNCTION,
+        C_RETURN,
+        C_INVALID
+    };
+
     static const std::unordered_map<std::string, int> SegmentPointerAddresses;
     static const std::unordered_map<std::string, std::string> ArithmeticUnaryOperators;
     static const std::unordered_map<std::string, std::string> ArithmeticBinaryOperators;
     static const std::unordered_map<std::string, std::string> ArithmeticCompareJumps;
 
     static const std::set<std::string> validCommands;
-    
+
     VMSpecifications() = delete;
 };
 
