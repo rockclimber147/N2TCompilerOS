d2a05d71bcabd49dcc41017e5c1905cfc1ba98ae
Daylen Smith <daylencsmith@gmail.com>
2026-03-21 18:12:46 +0000

    split test files

--- test/Emulators/VMEmulator/PushPopArithmeticTest.cpp
+++ test/Emulators/VMEmulator/PushPopArithmeticTest.cpp
@@ -1,70 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include "Emulators/VMEmulator/VMEmulator.hpp" 
 
-TEST_CASE("VM Decoder: Arithmetic", "[decoder]") {
-    VMEmulator vm;
-
-    SECTION("Binary Operations") {
-        std::vector<std::string> mathOps = {"add", "sub"};
-        for (const auto& op : mathOps) {
-            auto d = vm.decode(op);
-            REQUIRE(d.type == InstructionType::BINARY_ARITHMETIC);
-            REQUIRE(d.command == op);
-        }
-
-        std::vector<std::string> logicOps = {"and", "or"};
-        for (const auto& op : logicOps) {
-            auto d = vm.decode(op);
-            REQUIRE(d.type == InstructionType::BINARY_ARITHMETIC);
-            REQUIRE(d.command == op);
-        }
-        
-        std::vector<std::string> compOps = {"eq", "gt", "lt"};
-        for (const auto& op : compOps) {
-            auto d = vm.decode(op);
-            REQUIRE(d.type == InstructionType::BINARY_ARITHMETIC);
-            REQUIRE(d.command == op);
-        }
-    }
-
-    SECTION("Unary Operations") {
-        std::vector<std::string> unaryOps = {"neg", "not"};
-        for (const auto& op : unaryOps) {
-            auto d = vm.decode(op);
-            REQUIRE(d.type == InstructionType::UNARY_ARITHMETIC);
-            REQUIRE(d.command == op);
-        }
-    }
-}
-
-TEST_CASE("VM Decoder: Push and Pop", "[decoder]") {
-    VMEmulator vm;
-
-    SECTION("Valid Push Commands") {
-        auto d = vm.decode("push constant 42");
-        REQUIRE(d.type == InstructionType::PUSH);
-        REQUIRE(d.segment == Segment::CONSTANT);
-        REQUIRE(d.value == 42);
-
-        d = vm.decode("push local 0");
-        REQUIRE(d.type == InstructionType::PUSH);
-        REQUIRE(d.segment == Segment::LOCAL);
-        REQUIRE(d.value == 0);
-    }
-
-    SECTION("Valid Pop Commands") {
-        auto d = vm.decode("pop temp 7");
-        REQUIRE(d.type == InstructionType::POP);
-        REQUIRE(d.segment == Segment::TEMP);
-        REQUIRE(d.value == 7);
-
-        d = vm.decode("pop pointer 1");
-        REQUIRE(d.type == InstructionType::POP);
-        REQUIRE(d.segment == Segment::POINTER);
-        REQUIRE(d.value == 1);
-    }
-}
-
 TEST_CASE("VM Stack Arithmetic", "[arithmetic]") {
     VMEmulator vm;
     
@@ -75,7 +11,7 @@ TEST_CASE("VM Stack Arithmetic", "[arithmetic]") {
         vm.executeNextInstruction(); // add
         
         // Result should be at RAM[256], SP should be 257
-        REQUIRE(vm.peek(0) == 257);
+        REQUIRE(vm.peek(vm.STACK_POINTER) == 257);
         REQUIRE(vm.peek(256) == 15);
     }
 
